ae32e4ddd185176554cdb5ca334f23bfe5ce9b59
KyloDren <drennen@email.arizona.edu>
2024-10-23 18:36:31 -0700

    cleaning up beforeEach

--- src/router/index.js
+++ src/router/index.js
@@ -44,24 +44,25 @@ router.beforeEach(async (to, from) => {
 	        
 	        const params = new URLSearchParams(window.location.search)
 	        const ticket = params.get('ticket')
+	        const webAuthURL = "https://webauth.arizona.edu/webauth/login?service="
 	        let location = to.path
-	        let serviceURL = determineServiceURL()
+	        let serviceURL = window.location.origin
 
 	        if (!ticket) {
-	            redirectToWebAuth(serviceURL + location) 
+	        	window.location.replace(webAuthURL + serviceURL + location)
 	        }
 
-	        const tokenResult = await user.getToken({ ticket, location, serviceURL })
+	        let tokenResult = await user.getToken({ ticket, location, serviceURL })
 			if (!tokenResult) { return '/ServiceDown'}
 	        cleanUpURL()
 	        window.location.reload()
 	    }
 
-	    // INITIALIZE/GET ANY DATA SUCH AS RIGHTS NEEDED BEFORE FURTHER ROUTING AND FETCHES
-		
+	    // INITIALIZE/GET ANY DATA SUCH AS RIGHTS NEEDED BEFORE FURTHER ROUTING AND FETCHES		
 	    await user.initialize()
 		
 	    // EXAMPLE OF HOW TO GATE OFF ROUTES BASED ON USER RIGHTS
+	    // THESE ROUTES WILL BE REDIRECTED TO HOME IF NOT SUPERUSER
 	    if ((to.name === 'Usage' || to.name === 'Admin') && !user.isSuperUser) {
 	       return '/'
 	    } else {
@@ -69,22 +70,6 @@ router.beforeEach(async (to, from) => {
 	    }
 		
 })
-
-// SET THE BETA AND PRODUCTION SERVER URLS FOR WEBAUTH TO RETURN TO.
-// THESE ARE SWITCHED BASED ON 'VITE_APP_DEPLOYMENT' IN .ENV FILE
-function determineServiceURL() {
-    if (process.env.NODE_ENV === 'development') {
-        return "http://localhost:" + window.location.port
-    } else if (import.meta.env.VITE_APP_DEPLOYMENT === 'beta') {
-        return "https://beta.apps.ufs.arizona.edu/templateApp"
-    } else {
-        return "https://apps.ba.arizona.edu/templateApp"
-    }
-}
-
-function redirectToWebAuth(url) {
-    window.location.replace('https://webauth.arizona.edu/webauth/login?service=' + url)
-}
 
 function cleanUpURL() {
     let hostName = window.location.href.slice(0, window.location.href.indexOf("?"))
