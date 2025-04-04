5ea721269d5b72988e286d111a6eb157df7708df
KyloDren <drennen@email.arizona.edu>
2025-04-04 14:24:47 -0700

    added all the api urls.  more notes, added fetchSecurityTest, moved some things from ui to api made more sense

--- src/stores/api.js
+++ src/stores/api.js
@@ -2,32 +2,58 @@ import { defineStore } from 'pinia'
 import { user, ui } from '../stores'
 
 // API.JS
-// DATASTORE AND ACTIONS SPECIFIC TO THIS APPLICATION'S DATA
+// This store is used for external API definitions
+// They might just set a state variable for convenience, but should not obscure any important logic. 
 
 export const useApi = defineStore('api', {
-state: () => ({ 
+state: () => ({   
+    useDevDatabase: import.meta.env.VITE_APP_DEPLOYMENT === 'beta' ? true : false,  // Sets default database, can be switched at runtime by the user.  
+
+    useLocalAPI: false,
+    localApiUrl:    "http://localhost:3000/api",                        // Local API for development/testing (if using a local backend with this)
+    commonApiUrl:   "https://api.ba.arizona.edu/common",                // Common API for shared functionality (like user profile, etc)    
+    prodApiUrl:     "https://api.ba.arizona.edu/templateApp/api",       // Production API for this application (templateApp)
+    betaApiUrl:     "https://beta.api.ba.arizona.edu/templateApp/api",  // Beta API for this application (templateApp)
+
     buildings: null,
-    useBetaAPI: false,
-    commonApiUrl: "https://api.ba.arizona.edu/common"
 }),
 
 getters: {
-    apiURL() {
-        // Example of Switching APIs
-        /*return this.useBetaAPI ? 'https://api.ba.arizona.edu/betalyft' :
-        'https://api.ba.arizona.edu/lyft'*/
-    },
+    // Allows convenient switching of API using combinations of 'useLocalAPI' and VITE_APP_DEPLOYMENT
+    apiUrl() {
+        if (this.useLocalAPI) return this.localApiUrl;
+        return import.meta.env.VITE_APP_DEPLOYMENT === 'beta' ? this.betaApiUrl : this.prodApiUrl;
+      },
 
-    headers: (state) => {
+    // Default header
+      headers: (state) => {
         return {
             'Authorization':    user.token,
-            'database':         ui.getDatabase,
+            'database':         state.getDatabase,  // This can be implemented on the backned to switch databases based on the value of this header (dev vs not_dev)
             'content-type':     'application/json',
         }
     },
+
+    getDatabase: (state) => {
+        return state.useDevDatabase == true ? 'dev' : 'not_dev'
+    },
+
+    appId: (state) => {
+        return import.meta.env.VITE_APP_DEPLOYMENT === 'beta' 
+          ? import.meta.env.VITE_APP_ID_BETA 
+          : import.meta.env.VITE_APP_ID_PROD
+    },
 },
 
-actions: {    
+actions: {   
+    // Security Test
+    fetchSecurityTest() {
+        console.log("Action: Check Secure Endpoint")
+        return fetch(this.apiUrl + '/secure', { headers: this.headers })
+          .then(response => response.json())
+          .then(data => console.log("Secure Endpoint:", data))
+      },
+
     // EXAMPLE FETCH
     // Buildings from Space Database
     fetchBuildings () {
@@ -37,9 +63,9 @@ actions: {
         return fetch('https://api.pdc.arizona.edu/buildings', { headers: this.headers })
         .then(response => response.json())
         .then(data => { 
-            this.buildings = data
             console.log("Example of Return Confirmation")
             console.log("Buildings Fetched", data)
+            return data            
         })
     },
 },
